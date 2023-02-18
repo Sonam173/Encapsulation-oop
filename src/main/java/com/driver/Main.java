3567383fcb8aff04fa35e49cdb87ac1328e585c6
Sonam <sonamsahni425@gmail.com>
2023-02-18 15:07:09 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args)
+    {
+        RWonly obj = new RWonly();
+        obj.setName("Sonam");
+        System.out.println(obj.getName());
+    }
   
 }
